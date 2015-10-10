85cc16a539ee5e598ca7936933cbb48289214b85
Rosalind Cooper <rosalindleonora.cooper@mail.utoronto.ca>
2015-10-10 11:07:59 -0400

    push to repo

--- exercise2.py
+++ exercise2.py
@@ -29,3 +29,26 @@ def name_that_shape():
 
 
 name_that_shape()
+
+#!/usr/bin/env python
+# I am ignoring everything above this line
+# this was written and tested in Python 3. I still need to change input to raw_input.
+shape = int(input("Please enter the number of sides of your polygon: "))
+if shape < 3 or shape > 10:
+    print("Error")
+elif shape == 3:
+    print("your polygon is a triangle")
+elif shape == 4:
+    print("a square!")
+elif shape == 5:
+    print("pentagon")
+elif shape == 6:
+    print("hexagonn")
+elif shape == 7:
+    print("heptagon")
+elif shape == 8:
+    print("octagon")
+elif shape == 9:
+    print("nonagon")
+elif shape == 10:
+    print("decagon")
