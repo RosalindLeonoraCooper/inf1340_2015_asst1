670a4e0d822ff85b14f6a0b34945159c2ec84bea
Rosalind Cooper <rosalindleonora.cooper@mail.utoronto.ca>
2015-10-10 11:11:42 -0400

    push to repo

--- exercise3.py
+++ exercise3.py
@@ -30,3 +30,35 @@ def diagnose_car():
 
 
 diagnose_car()
+
+#!/usr/bin/env python
+# I am ignoring everything above this line.
+# 1340 exercise 3, assignment 1
+# trouble shooting car issue with expert system
+# Written and tested in python 3. need to change input to raw_input
+question1 = input("Is the car silent when you turn the key? (yes/no) ")
+if question1 == 'yes':
+    question1_1 = input("Are the battery terminals corroded? (yes/no) ")
+    if question1_1 == 'yes':
+        print("Clean the terminals and try starting again.")
+    elif question1_1 == 'no':
+        print("Replace cables and try again.")
+elif question1 == 'no':
+    question1_2 = input("Does the car make a clicking noise? (yes/no) ")
+    if question1_2 == 'yes':
+        print("Replace the battery.")
+    elif question1_2 == 'no':
+        question2 = input("Does the car crank up but fail to start? (yes/no) ")
+        if question2 == 'yes':
+            print("Check spark plug connections")
+        elif question2 == 'no':
+            question3 = input("Does the engine start and then die? (yes/no) ")
+            if question3 == 'no':
+                print("System has no response.")
+            elif question3 == 'yes':
+                question4 = input("Does your car have fuel injection? (yes/no) ")
+                if question4 == 'no':
+                    print("Check to ensure the choke is opening and closing.")
+                elif question4 == 'yes':
+                    print("Get it in for service.")
+
